3e12881685917451a89d5a065350416339f2725d
Thierry Schlegel <thierryschlegel@gmail.com>
2018-12-07 16:18:44 +0100

    test

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import Image from '../components/image'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Hi people 23</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    
+    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage
